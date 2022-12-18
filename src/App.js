d2050f090128777aea13b7315a5ece927d3e8fde
ryans <rsweeney@smu.edu>
2022-12-18 12:20:30 -0500

    Created NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+import {Button} from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+            <NavBar/>
+            <h1>This page is under construction</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Routes>
+                <Route path="/" exact/>}/>
+            </Routes>
+        </Router>
+
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {faTypo3} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom";
+import {faBars, faTimes} from "@fortawesome/free-solid-svg-icons";
+import {Button} from "./Button";
+import './NavBar.css';
+
+function NavBar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    ryansweeney.org
+                </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                    <FontAwesomeIcon icon={click ? faTimes : faBars} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-links" onClick={closeMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/custom-computers" className="nav-links" onClick={closeMenu}>
+                            Custom Computers
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/projects" className="nav-links" onClick={closeMenu}>
+                            Projects
+                        </Link>
+                    </li>
+                </ul>
+                </div>
+        </nav>
+    );
+}
+
+export default NavBar;
